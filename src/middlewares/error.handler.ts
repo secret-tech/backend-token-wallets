808b76354166d9e24b03bc34f11a10412fd3139e
Popov Ivan <lord.iv@mail.ru>
2018-07-13 22:43:39 +0800

    Fixed error: "req.acceptsLanguages is not a function"

--- src/middlewares/error.handler.ts
+++ src/middlewares/error.handler.ts
@@ -11,7 +11,7 @@ const logger = Logger.getInstance('ERROR_HANDLER');
 
 export default function defaultExceptionHandle(err: ErrorWithFields, req: Request, res: Response, next: NextFunction): void {
   let status;
-  const lang = req.acceptsLanguages() || 'en';
+  const lang = 'en';
   const langPath = `../resources/locales/${lang}/errors.json`;
   const translations = fs.existsSync(langPath) ? require(langPath) : null;
 
